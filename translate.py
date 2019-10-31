a87c6d31a33f3f7d9938f9a1b63863b51abbdb65
Nanson, Celien <cesliens@gmail.com>
2019-10-31 14:46:26 +0100

    Add a translate function

--- translate.py
+++ translate.py
@@ -1,4 +1,5 @@
 import boto3
+import time
 
 class VideoFile():
   def __init__(self):
@@ -11,5 +12,24 @@ def upload_audio_to_s3():
   s3 = boto3.resource('s3')
   s3.meta.client.upload_file('./audio_trimmed.mp3', 's3-ec1-app-bucket', 'audio.mp3')
 
+def transcribe():
+  transcribe = boto3.client('transcribe')
+  job_name = "test_tranlate"
+  job_uri = "https://s3-ec1-app-bucket.s3.eu-central-1.amazonaws.com/audio.mp3"
+  transcribe.start_transcription_job(
+    TranscriptionJobName=job_name,
+    Media={'MediaFileUri': job_uri},
+    MediaFormat='mp3',
+    LanguageCode='en-US'
+  )
+  while True:
+    status = transcribe.get_transcription_job(TranscriptionJobName=job_name)
+    if status['TranscriptionJob']['TranscriptionJobStatus'] in ['COMPLETED', 'FAILED']:
+        break
+    print("Not ready yet...")
+    time.sleep(5)
+  print(status)
 
-upload_audio_to_s3()
+#upload_audio_to_s3()
+transcribe()
+
