777e1a49804e71946711c42fc6ac6c90a441dbc4
Nanson, Celien <cesliens@gmail.com>
2019-11-06 18:36:38 +0100

    Locally save result of the Translation

--- translate.py
+++ translate.py
@@ -1,3 +1,5 @@
+import shutil
+import urllib.request
 import boto3
 import time
 import string
@@ -11,12 +13,13 @@ class VideoFile():
     return 0
 
 class Transcribe():
-  def __init__(self, path_audio_input, region, bucket):
+  def __init__(self, path_audio_input, path_transcribe_result_output, region, bucket):
     self._transcribe_job_name = self._randomize_job_name()
     self._s3_file_name = self._randomize_job_name()+'.mp3'
     self._path_audio_input = path_audio_input
     self._aws_region = region
     self._bucket_name = bucket
+    self._path_transcribe_result_output = path_transcribe_result_output
 
   def _randomize_job_name(self):
     letters = string.ascii_lowercase
@@ -30,7 +33,6 @@ class Transcribe():
     transcribe = boto3.client('transcribe')
     job_name = self._transcribe_job_name
     job_uri = 'https://'+self._bucket_name+'.s3.'+self._aws_region+'.amazonaws.com/'+self._s3_file_name
-    print(job_uri)
     transcribe.start_transcription_job(
         TranscriptionJobName=job_name,
         Media={'MediaFileUri': job_uri},
@@ -42,12 +44,16 @@ class Transcribe():
       status = transcribe.get_transcription_job(TranscriptionJobName=job_name)
       if status['TranscriptionJob']['TranscriptionJobStatus'] in ['COMPLETED', 'FAILED']:
           break
-      print("Not ready yet...")
+      print('Translating audio...')
       time.sleep(5)
 
     response_url = transcribe.get_transcription_job(TranscriptionJobName = self._transcribe_job_name)
-    print(response_url['TranscriptionJob']['Transcript']['TranscriptFileUri'])
-    #boto3.client('s3').get_object(Bucket=self._bucket_name, Key='')
+    translated_file_url = response_url['TranscriptionJob']['Transcript']['TranscriptFileUri']
+
+    # Download the result of the transcribe job locally
+    with urllib.request.urlopen(translated_file_url) as response, open(self._path_transcribe_result_output, 'wb') as out_file:
+      shutil.copyfileobj(response, out_file)
+    print('Translation done and saved under: '+self._path_transcribe_result_output)
 
   def run(self):
     self._upload_audio_to_s3()
@@ -55,4 +61,4 @@ class Transcribe():
 
 
 if __name__ == '__main__':
-  Transcribe('./audio_trimmed.mp3', 'eu-central-1', 's3-ec1-app-bucket').run()
+  Transcribe('./audio_trimmed.mp3', './transcribe_result.json', 'eu-central-1', 's3-ec1-app-bucket').run()
