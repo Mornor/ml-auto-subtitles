4c8cde2455a5d8d5f970cf5d9980ed8eb475bbf9
Nanson, Celien <cesliens@gmail.com>
2019-11-21 13:34:22 +0100

    Harmonize code logic

--- code/transcribe.py
+++ code/transcribe.py
@@ -6,29 +6,29 @@ import shutil
 
 class Transcribe():
     def __init__(self, config):
-        self.utils = utils.Utils()
-        self.aws_interact = aws_interact.AwsInteract()
-        self.transcribe_job_name = self.utils._randomize_job_name()
-        self.s3_file_name = self.utils._randomize_job_name()+'.mp3'
-        self.path_audio_input = config['path_audio_input']
-        self.aws_region = config['aws_region']
-        self.bucket_name = config['bucket']
+        self._utils = utils.Utils()
+        self._aws_interact = aws_interact.AwsInteract()
+        self._transcribe_job_name = self._utils._randomize_job_name()
+        self._s3_file_name = self._utils._randomize_job_name()+'.mp3'
+        self._path_audio_input = config['path_audio_input']
+        self._aws_region = config['aws_region']
+        self._bucket_name = config['bucket']
         self._path_transcribe_result_output = './transcribe_result.json'
         self._path_subtitle_file = config['path_srt_output']
 
     # Upload audio file to S3 Bucket
     def _upload_audio_to_s3(self):
-        self.aws_interact._upload_audio_to_s3(self.path_audio_input, self.bucket_name, self.s3_file_name)
+        self._aws_interact._upload_audio_to_s3(self._path_audio_input, self._bucket_name, self._s3_file_name)
 
     # Run the AWS Transcribe job, download and locally save the result
     def _transcribe_and_save(self):
-        transcribe_file_url = self.aws_interact._transribe(self.transcribe_job_name, self.aws_region, self.bucket_name, self.s3_file_name)
-        self.aws_interact._download_from_url(transcribe_file_url, self._path_transcribe_result_output)
+        transcribe_file_url = self._aws_interact._transribe(self._transcribe_job_name, self._aws_region, self._bucket_name, self._s3_file_name)
+        self._aws_interact._download_from_url(transcribe_file_url, self._path_transcribe_result_output)
 
     # Parse the Transcribe result JSON file into .srt format
     def _parse_transcribe_result(self):
-        phrases = self.utils._parse_transcribe(self._path_transcribe_result_output)
-        self.utils._write_srt_file(phrases, self._path_subtitle_file)
+        phrases = self._utils._parse_transcribe(self._path_transcribe_result_output)
+        self._utils._write_srt_file(phrases, self._path_subtitle_file)
 
     def run(self):
         self._upload_audio_to_s3()
